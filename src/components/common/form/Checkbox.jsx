8996f6840b019a749fe206575bc0d58cc77efe40
김선경 <mimcom43@gmail.com>
2025-10-28 12:18:31 +0900

    Add : StoreCRUD 초기 작업

--- src/components/common/form/Checkbox.jsx
+++ src/components/common/form/Checkbox.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-function Checkbox(props) {
-  return <div></div>;
+function Checkbox({ label, options = [], selected = [], onChange }) {
+  return (
+    <>
+      {label && <p className="checkbox-label">{label}</p>}
+      <div className="checkbox-field">
+        {options.map((option) => (
+          <label key={option} className="checkbox-input">
+            <input
+              type="checkbox"
+              value={option}
+              checked={selected.includes(option)} // 부모가 상태 관리
+              onChange={() => onChange(option)} // 부모로 이벤트 전달
+            />
+            <span>{option}</span>
+          </label>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Checkbox;
