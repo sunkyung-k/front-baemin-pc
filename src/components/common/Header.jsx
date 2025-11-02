f55b4558269c4b431ffcc76944beb493c2710af2
김선경 <mimcom43@gmail.com>
2025-11-02 16:02:34 +0900

    Add : 공통 컴포넌트 > header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router";
+import { FaHeart, FaUser, FaBookOpen, FaBars, FaTimes } from "react-icons/fa";
+import { RiEBike2Fill } from "react-icons/ri";
+import { authStore } from "@/store/authStore";
 
-function Header(props) {
+function Header() {
+  const { isAuthenticated, clearAuth, getUserRole } = authStore();
+  const userName = authStore((state) => state.userName);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    clearAuth();
+    localStorage.removeItem("auth-info");
+    navigate("/login");
+  };
+
   return (
-    <>
-      <header className="header">ddd</header>
-    </>
+    <header className="header">
+      <div className="header-inner">
+        <div className="logo">
+          <NavLink to="/">배민PC</NavLink>
+        </div>
+
+        <div className="user-menu">
+          {isAuthenticated() ? (
+            <>
+              <span>{userName}님, 안녕하세요.</span>
+              <button className="btn btn-round" onClick={handleLogout}>
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <NavLink to="/login" className="btn btn-round">
+              로그인
+            </NavLink>
+          )}
+        </div>
+      </div>
+
+      <nav className="nav">
+        {isAuthenticated() && getUserRole() === "ROLE_ADMIN" ? (
+          <NavLink to="/admin">관리자 페이지</NavLink>
+        ) : (
+          <>
+            <NavLink to="/order">
+              <RiEBike2Fill /> 주문 현황
+            </NavLink>
+            <NavLink to="/favorite">
+              <FaHeart /> 찜
+            </NavLink>
+            <NavLink to="/mypage">
+              <FaUser /> 마이페이지
+            </NavLink>
+          </>
+        )}
+      </nav>
+    </header>
   );
 }
 
