e7f94ce3969238bc33fcaee040dd40e47b82c6f8
김선경 <mimcom43@gmail.com>
2025-11-04 16:47:47 +0900

    Add : 공통 > LoadingSpinner

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./LoadingSpinner.module.scss";
+
+/**
+ * 💫 공용 로딩 스피너 컴포넌트
+ * - 페이지 전용, 버튼 내부용, 섹션용으로 모두 재활용 가능
+ *
+ * @param {number} size - 원 크기(px)
+ * @param {string} message - 로딩 메시지
+ * @param {boolean} fullscreen - 전체 화면 중앙 배치 여부
+ */
+function LoadingSpinner({
+  size = 48,
+  message = "불러오는 중...",
+  fullscreen = false,
+}) {
+  return (
+    <div className={`${styles.wrapper} ${fullscreen ? styles.fullscreen : ""}`}>
+      <div
+        className={styles.spinner}
+        style={{ width: size, height: size }}
+      ></div>
+      {message && <p className={styles.text}>{message}</p>}
+    </div>
+  );
+}
+
+export default LoadingSpinner;
