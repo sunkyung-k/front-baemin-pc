41da620e9e91c841b949a1ab4298903ddbb1b8a6
홍려경 <dhnp3879@gmail.com>
2025-11-05 17:16:58 +0900

    Feat : 회원가입 > 정규식 스키마 문구 수정

--- src/pages/join/Join.jsx
+++ src/pages/join/Join.jsx
@@ -28,7 +28,10 @@ const schema = yup.object().shape({
   password: yup
     .string()
     .required("비밀번호를 입력해주세요.")
-    .matches(PW_REGEX, "비밀번호는 영문과 숫자를 포함해야 합니다."),
+    .matches(
+      PW_REGEX,
+      "비밀번호는 영문과 숫자 포함해 모두 포함해 최소 8자 이상으로 입력해주세요."
+    ),
   confirmPassword: yup
     .string()
     .required("비밀번호를 다시 입력해주세요.")
